80cbcf213cc58b54f044312a6fd02d90761a0ff5
stoennnnn <2672575920@qq.com>
2019-02-11 16:57:22 +0800

    启动类

---
+++ src/main/java/com/zql/APP.java
@@ -0,0 +1,15 @@
+package com.zql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 26725 on 2019/2/11.
+ */
+@SpringBootApplication
+public class APP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APP.class,args);
+    }
+}
