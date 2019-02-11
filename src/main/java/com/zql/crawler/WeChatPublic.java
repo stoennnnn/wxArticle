dc06db17ffa571b8293cc4fa5efceef2ce02d44b
zhangqilei <2672575920@qq.com>
2019-02-12 00:53:00 +0800

    解析数据

--- src/main/java/com/zql/crawler/WeChatPublic.java
+++ src/main/java/com/zql/crawler/WeChatPublic.java
@@ -1,32 +1,50 @@
 package com.zql.crawler;
 
+import com.zql.dto.AppMsgExtInfo;
+import com.zql.utils.JsonUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class WeChatPublic {
+    @Value("${public.accountNumber}")
+    private String accountNumber;
     public void getFirst(){
         String baseUrl = "http://weixin.sogou.com/weixin?type=1&ie=utf8&query=";//搜狗微信的基础地址
-        String searchUrl = baseUrl + "javastack";//加上安泰科现货价格的微信公总号
+        String searchUrl = baseUrl + accountNumber;//加上安泰科现货价格的微信公总号
         Document document = getDocument(searchUrl);
         String listUrl = document.select(".tit a").attr("href");//查询到所有列表信息的url
         System.out.println(listUrl);
         System.out.println("-----------------------");
         Document doc = getDocument(listUrl);
         //截取前10条推送信息的json中的list数据
-        String jsonList = doc.html().split("var msgList = ")[1].split("seajs.use")[0].trim();
+        String content = doc.html().split("var msgList = ")[1].split("seajs.use")[0].trim();
+        String contentJson = content.substring(0, content.lastIndexOf(";"));
+        //json转list
+        List<AppMsgExtInfo> appMsgExtInfoList = JsonUtil.toList(contentJson);
         //截取出来第一条信息的url
-        String url = jsonList.split("content_url\":\"")[1].split("\",\"copyright_stat")[0].replaceAll("amp;", "");
+        String url = contentJson.split("content_url\":\"")[1].split("\",\"copyright_stat")[0].replaceAll("amp;", "");
         if(url.startsWith("/s")){//如果url以/s开头，处理成http格式
             url = "http://mp.weixin.qq.com" + url;
         }
         System.out.println(url);
         //获取第一条推送信息的标题
-        String title = jsonList.split("title\":\"")[1].split("\"},\"comm_msg_info")[0];
+        String title = contentJson.split("title\":\"")[1].split("\"},\"comm_msg_info")[0];
         System.out.println(title);
+//        private Integer accountId;
+//        private String articleTitle;
+//        private String articleDigest;
+//        private String articleContentUrl;
+//        private String articleCoverUrl;
+//        private Date articleDatetime;
+//        private String accountNickname;
+//        private String accountNumber;
+//        private String accountDesc;
     }
 
     /**
